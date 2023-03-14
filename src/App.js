4898c8f6be3e2ab21e1a073bdab4db49f5e50507
drickpdr98 <dravinsookraj@gmail.com>
2023-03-13 22:52:08 -0400

    comments

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [usersList, setUsersList] = useState([]);
 
   const addUserHandler = (userName, userAge) => {
+    // understand what this does
     setUsersList((prevUsers) => [
       ...prevUsers,
       { name: userName, age: userAge, id: Math.random().toString },
